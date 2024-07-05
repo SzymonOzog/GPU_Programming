4840865a7e92f290c0d8f4c694b1adc720e7d487
SzymonOzog <szymon.ozog@gmail.com>
2024-07-05 11:38:18 +0200

    GPU vs CPU code comparison

--- manim_scripts/1_CPU_vs_GPU.py
+++ manim_scripts/1_CPU_vs_GPU.py
@@ -2,13 +2,13 @@ from manim import *
 from manim_voiceover import VoiceoverScene
 from manim_voiceover.services.gtts import GTTSService
 from manim_voiceover.services.recorder import RecorderService
+from manim.mobject.text.text_mobject import remove_invisible_chars
 
 class Introduction(VoiceoverScene, ThreeDScene):
   def construct(self):
     self.set_speech_service(
         GTTSService()
-        # RecorderService(trim_buffer_end=50, trim_silence_threshold=-80, transcription_model=None)
-        )
+        # RecorderService(trim_buffer_end=50, trim_silence_threshold=-80, transcription_model=None))
 
     title = Text("GPU programming", font_size=72).shift(2*UP)
     with self.voiceover(text="Hello and welcome to episode 1 in the series on GPU programming") as trk:
@@ -212,5 +212,130 @@ class Introduction(VoiceoverScene, ThreeDScene):
       part.add_to_back(dot)
       part.move_to(gpu_details[3], aligned_edge=LEFT)
       self.play(Transform(gpu_details[3], part))
+
+    self.wait(1)
+
+    with self.voiceover(text="""Let's get into some examples of when a GPU can vastly outperform the CPU""") as trk:
+      for i in range(4):
+        self.play(Unwrite(cpu_details[3-i], run_time=trk.duration/4), Unwrite(gpu_details[3-i], run_time=trk.duration/4))
+
+    n = 6
+    v2 = Matrix([*[[f"b_{i}"] for i in range(n-2)], ["\\vdots"], ["b_n"]], element_alignment_corner=ORIGIN).shift(DOWN)
+    plus = Tex("+").next_to(v2, LEFT)
+    v1 = Matrix([*[[f"a_{i}"] for i in range(n-2)], ["\\vdots"], ["a_n"]], element_alignment_corner=ORIGIN).next_to(plus, LEFT)
+    eq = Tex("=").next_to(v2, RIGHT)
+    v3 = Matrix([*[["?"] for i in range(n-2)], ["\\vdots"], ["?"]], element_alignment_corner=ORIGIN).next_to(eq, RIGHT)
+    with self.voiceover(text="""One such example of a highly parralelizable algorithm is vector addition""") as trk:
+
+      self.play(*[Create(x) for x in [v1, v2, v3, plus, eq]])
+
+    def create_m(m):
+      ret = []
+      for i in range(n):
+        if i == n - 2:
+          ret.append(["\\vdots"])
+        elif m == n - 2:
+          ret.append(["?"])
+        elif i <= m:
+          if i == n-1:
+            ret.append([f"a_n + b_n"])
+          else:
+            ret.append([f"a_{i} + b_{i}"])
+        else:
+          ret.append(["?"])
+      return ret
+    self.wait(1)
+    # self.play(*[x.animate.shift(RIGHT) for x in [v1, v2, v3, plus, eq]])
+    cpu_code = """void add(int n , float* a, float* b, float* c)
+{
+  for (int i = 0; i<n; i++)
+  {
+    c[i] = a[i] + b[i];
+  }
+}"""
+    cpu_code_obj = Code(code=cpu_code, tab_width=2, language="c", font_size=14, background="rectangle", line_no_buff=0.1, corner_radius=0.1).next_to(v2, UP)
+    with self.voiceover(text="""The way that you would typically do this on a CPU is to iterate over all of the values in our input vecor,
+                        add them together and store them in the third vector""") as trk:
+      self.play(Create(cpu_code_obj))
+      for i in range(n):
+        if i == n-2: continue
+        self.play(FadeOut(v1.get_entries()[i].copy(), target_position=v3.get_entries()[i]), 
+                  FadeOut(v2.get_entries()[i].copy(), target_position=v3.get_entries()[i]), 
+                  Transform(v3, Matrix(create_m(i), element_alignment_corner=ORIGIN).next_to(eq, RIGHT)))
+
+    with self.voiceover(text="""But we can clearly see that the output does not depend on any of the previous iterations""") as trk:
+      self.play(Transform(v3, Matrix(create_m(n-2), element_alignment_corner=ORIGIN).next_to(eq, RIGHT)))
+
+    with self.voiceover(text="""So what we want to do, is just run everything in parallel""") as trk:
+      self.play(*[FadeOut(v1.get_entries()[i].copy(), target_position=v3.get_entries()[i]) for i in range(n) if i != n-2], 
+                *[FadeOut(v2.get_entries()[i].copy(), target_position=v3.get_entries()[i]) for i in range(n) if i != n-2],
+                Transform(v3, Matrix(create_m(n-1), element_alignment_corner=ORIGIN).next_to(eq, RIGHT)))
+
+    gpu_code = """__global__ void add(int n , float* a, float* b, float* c)
+{
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n)
+  {
+    c[i] = a[i] + b[i];
+  }
+}"""
+    gpu_code_obj = Code(code=gpu_code, tab_width=2, language="c", font_size=14, line_no_buff=0.1, corner_radius=0.1).next_to(v2, UP)
+    gpu_code_obj.code = remove_invisible_chars(gpu_code_obj.code)
+    with self.voiceover(text="""Here is the GPU code that let's us achieve this""") as trk:
+      self.play(Transform(cpu_code_obj, gpu_code_obj))
+    self.wait(1)
+    with self.voiceover(text="""First difference that we can notice is the global keyword - this is just an information for the
+                        compiler that this function is to be run on the GPU""") as trk:
+      sw = SurroundingRectangle(gpu_code_obj.code[0][0:10], buff=0.03, stroke_width=2, fill_opacity=0.3)
+      self.play(Create(sw))
+    self.wait(1)
+    with self.voiceover(text="""Next difference is the index calculation, instead of a for loop. Since the code will run in parallel there will be multiple 
+                        instances of it created, and every one needs to be able to calculate which element in our vectors it is supposed to work on""") as trk:
+      self.play(Transform(sw, SurroundingRectangle(gpu_code_obj.code[2], buff=0.03, stroke_width=2, fill_opacity=0.3)))
+
+    fs = 32
+    block1 = SurroundingRectangle(VGroup(*v1.get_entries()[:2])).shift(1.5*LEFT)
+    t1 = []
+    t1.append(Tex("$T_0$", font_size=fs).move_to(block1.get_corner(UP)+DOWN*0.2, aligned_edge=UP))
+    t1.append(Tex("$T_1$", font_size=fs).move_to(block1.get_corner(DOWN)-DOWN*0.2, aligned_edge=DOWN))
+    t1.append(Tex("$B_0$", font_size=fs).next_to(block1, LEFT))
+
+    block2 = SurroundingRectangle(VGroup(*v1.get_entries()[2:4])).shift(1.5*LEFT)
+    t2 = []
+    t2.append(Tex("$T_0$", font_size=fs).move_to(block2.get_corner(UP)+DOWN*0.2, aligned_edge=UP))
+    t2.append(Tex("$T_1$", font_size=fs).move_to(block2.get_corner(DOWN)-DOWN*0.2, aligned_edge=DOWN))
+    t2.append(Tex("$B_1$", font_size=fs).next_to(block2, LEFT))
+
+    block3 = SurroundingRectangle(VGroup(*v1.get_entries()[4:])).shift(1.5*LEFT)
+    t3 = []
+    t3.append(Tex("$T_0$", font_size=fs).move_to(block3.get_corner(UP)+DOWN*0.2, aligned_edge=UP))
+    t3.append(Tex("$T_1$", font_size=fs).move_to(block3.get_corner(DOWN)-DOWN*0.2, aligned_edge=DOWN))
+    t3.append(Tex("$B_{\\frac{n}{2}}$", font_size=fs).next_to(block3, LEFT))
+    with self.voiceover(text="""The way CUDA achieves this is by using a concept of thread blocks. There will be a whole episode directed towards understanding them
+                        but for now you need to understand a few basic concepts""") as trk:
+      pass
+    self.wait(0.5)
+
+    with self.voiceover(text="""When we launch our Kernel, we need to specify how many blocks we will launch, and how many threads will each block contain""") as trk:
+      pass
+
+    with self.voiceover(text="""For exaple, when we launch n/2 blocks, each containing n threads, here's how the structure of our blocks will look like""") as trk:
+      self.play(Create(block1), *[Write(t) for t in t1])
+      self.play(Create(block2), *[Write(t) for t in t2])
+      self.play(Create(block3), *[Write(t) for t in t3])
+    with self.voiceover(text="""Each thread runs it's own version of the code with the appropriate block index and thread index. 
+                        Block dimensions are shared between the threads""") as trk:
+      pass
+    with self.voiceover(text="""It's left as an excercise to the watcher to verivy that each thread will calculate a different index""") as trk:
+      pass
+    self.wait(1)
+
+    with self.voiceover(text="""The last difference is checking if our index is inside our vector""") as trk:
+      self.play(Transform(sw, SurroundingRectangle(gpu_code_obj.code[3], buff=0.03, stroke_width=2, fill_opacity=0.3)))
+
+    with self.voiceover(text="""For example, if we had an uneaven number of elements, the alignment of blocks might look like this,
+                        where the last thread in the last block would read and write out of bouds, the check ensures that we don't do that""") as trk:
+      self.play(*[x.animate.shift(0.7*DOWN) for x in [block3]+t3])
+      self.play(t3[1].animate.set_color(RED))
 
     self.wait(1)
