e8694f1d3c7ada880579a5a18343b21b93f79e41
SzymonOzog <szymon.ozog@gmail.com>
2024-07-12 10:57:13 +0200

    Add single dim matmul

--- matmul.cu
+++ matmul.cu
@@ -11,46 +11,34 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
-__global__ void matmul_elem(int w, int h, float* a, float* b, float* c)
+__global__ void matmul_elem(int n, float* a, float* b, float* c)
 {
   int column = blockIdx.x*blockDim.x + threadIdx.x;
   int row = blockIdx.y*blockDim.y + threadIdx.y;
-  if (row < w && column < h)
+  if (row < n && column < n)
   {
-    for(int i = 0; i < w; i++)
+    float dot_prod = 0.f;
+    for(int i = 0; i < n; i++)
     {
-      c[row*w+column] += a[row*w + i] * b[i*w + column];
+      dot_prod += a[row*n + i] * b[i*n + column];
     }
+    c[row*n+column] = dot_prod;
   }
 }
 
-__global__ void matmul_row(int w, int h, float* a, float* b, float* c)
+__global__ void matmul_elem_onedim(int n, float* a, float* b, float* c)
 {
-  int row = blockIdx.y*blockDim.y + threadIdx.y;
-  if (row < w)
+  int idx = blockIdx.x*blockDim.x + threadIdx.x;
+  int row = idx/n;
+  int column = idx%n;
+  if (row < n && column < n)
   {
-    for(int i = 0; i < w; i++)
+    float dot_prod = 0.f;
+    for(int i = 0; i < n; i++)
     {
-      for(int j = 0; j < w; j++)
-      {
-        c[row*w+j] += a[row*h + i] * b[i*w + j];
-      }
+      dot_prod += a[row*n + i] * b[i*n + column];
     }
-  }
-}
-
-__global__ void matmul_col(int w, int h, float* a, float* b, float* c)
-{
-  int column = blockIdx.x*blockDim.x + threadIdx.x;
-  if (column < h)
-  {
-    for(int i = 0; i < w; i++)
-    {
-      for(int j = 0; j < w; j++)
-      {
-        c[j*w+column] += a[j*w + i] * b[i*w + column];
-      }
-    }
+    c[row*n+column] = dot_prod;
   }
 }
 
@@ -78,13 +66,11 @@ int main()
   float* b_d;
   float* c_d;
   float* d_d;
-  float* e_d;
 
   cudaMalloc((void**) &a_d, N*N*sizeof(float));
   cudaMalloc((void**) &b_d, N*N*sizeof(float));
   cudaMalloc((void**) &c_d, N*N*sizeof(float));
   cudaMalloc((void**) &d_d, N*N*sizeof(float));
-  cudaMalloc((void**) &e_d, N*N*sizeof(float));
 
   cudaMemcpy(a_d, a, N*N*sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(b_d, b, N*N*sizeof(float), cudaMemcpyHostToDevice);
@@ -92,43 +78,32 @@ int main()
   dim3 dimGrid(ceil(N/(float)BLOCK_SIZE), ceil(N/(float)BLOCK_SIZE), 1);
   dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
 
-  matmul_elem<<<dimGrid, dimBlock>>>(N, N, a_d, b_d, c_d);
+  matmul_elem<<<dimGrid, dimBlock>>>(N, a_d, b_d, c_d);
   gpuErrchk(cudaPeekAtLastError());
   gpuErrchk(cudaDeviceSynchronize());
 
   cudaMemcpy(c, c_d, N*N*sizeof(float), cudaMemcpyDeviceToHost);
 
-  dim3 dimGrid_r(1, ceil(N/(float)BLOCK_SIZE), 1);
-  dim3 dimBlock_r(1, BLOCK_SIZE, 1);
+  dim3 dimGrid_r(ceil(N*N/(float)BLOCK_SIZE), 1, 1);
+  dim3 dimBlock_r(BLOCK_SIZE, 1, 1);
 
-  matmul_row<<<dimGrid_r, dimBlock_r>>>(N, N, a_d, b_d, d_d);
+  matmul_elem_onedim<<<dimGrid, dimBlock>>>(N, a_d, b_d, c_d);
   gpuErrchk(cudaPeekAtLastError());
   gpuErrchk(cudaDeviceSynchronize());
 
   cudaMemcpy(d, d_d, N*N*sizeof(float), cudaMemcpyDeviceToHost);
 
-  dim3 dimGrid_c(ceil(N/(float)BLOCK_SIZE), 1, 1);
-  dim3 dimBlock_c(BLOCK_SIZE, 1, 1);
-
-  matmul_col<<<dimGrid_c, dimBlock_c>>>(N, N, a_d, b_d, e_d);
-  gpuErrchk(cudaPeekAtLastError());
-  gpuErrchk(cudaDeviceSynchronize());
-
-  cudaMemcpy(e, e_d, N*N*sizeof(float), cudaMemcpyDeviceToHost);
-  
   for (int i = 0; i<N; i++)
   {
     for (int j = 0; j<N; j++)
     {
       assert(c[i*N+j] == b[i*N+j]*2);
-      assert(d[i*N+j] == b[i*N+j]*2);
-      assert(e[i*N+j] == b[i*N+j]*2);
+      assert(d[i*N+j] == d[i*N+j]*2);
     }
   }
   cudaFree(a_d);
   cudaFree(b_d);
   cudaFree(c_d);
   cudaFree(d_d);
-  cudaFree(e_d);
   return 0;
 }
