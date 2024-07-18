27c05db13e4b4e5a72dc8fb8c123f204f427e869
SzymonOzog <szymon.ozog@gmail.com>
2024-07-18 12:42:42 +0200

    Modified neural network code from Alex

---
+++ manim_scripts/NN.py
@@ -0,0 +1,176 @@
+from manim import *
+import itertools as it
+# A customizable Sequential Neural Network
+# Copied and edited based on https://www.youtube.com/watch?v=HnIeAP--vWc
+class NeuralNetworkMobject(VGroup):
+    CONFIG = {
+        "neuron_radius": 0.1,
+        "neuron_to_neuron_buff": SMALL_BUFF,
+        "layer_to_layer_buff": LARGE_BUFF*2,
+        "output_neuron_color": WHITE,
+        "input_neuron_color": WHITE,
+        "hidden_layer_neuron_color": WHITE,
+        "neuron_stroke_width": 2,
+        "neuron_fill_color": GREEN,
+        "edge_color": LIGHT_GREY,
+        "edge_stroke_width": 2,
+        "edge_propogation_color": YELLOW,
+        "edge_propogation_time": 1,
+        "max_shown_neurons": [20,16,12,10],
+        "brace_for_large_layers": True,
+        "average_shown_activation_of_large_layer": True,
+        "include_output_labels": False,
+        "arrow": False,
+        "arrow_tip_size": 0.1,
+        "left_size": 1,
+        "neuron_fill_opacity": 1
+    }
+    # Constructor with parameters of the neurons in a list
+    def __init__(self, neural_network, *args, **kwargs):
+        VGroup.__init__(self, *args, **kwargs)
+        self.layer_sizes = neural_network
+        self.__dict__.update(self.CONFIG) 
+        self.neurons = []
+        self.edges = []
+        self.braces = []
+        self.add_neurons()
+        self.add_edges()
+        self.add_to_back(self.layers)
+        
+    # Helper method for constructor
+    def add_neurons(self):
+        layers = VGroup(*[
+            self.get_layer(size, index, self.max_shown_neurons[index])
+            for index, size in enumerate(self.layer_sizes)
+        ])
+        layers.arrange_submobjects(RIGHT, buff=self.layer_to_layer_buff)
+        self.layers = layers
+        if self.include_output_labels:
+            self.label_outputs_text()
+    # Helper method for constructor
+    def get_nn_fill_color(self, index):
+        if index == -1 or index == len(self.layer_sizes) - 1:
+            return self.output_neuron_color
+        if index == 0:
+            return self.input_neuron_color
+        else:
+            return self.hidden_layer_neuron_color
+    # Helper method for constructor
+    def get_layer(self, size, index=-1, max_shown_neurons=16):
+        layer = VGroup()
+        n_neurons = size
+        self.neurons.append([])
+        if n_neurons > max_shown_neurons:
+            n_neurons = max_shown_neurons
+        for x in range(n_neurons):
+          self.neurons[-1].append(Circle(
+                radius=self.neuron_radius,
+                stroke_color=self.get_nn_fill_color(index),
+                stroke_width=self.neuron_stroke_width,
+                fill_color=BLACK,
+                fill_opacity=self.neuron_fill_opacity,
+            ))
+        neurons = VGroup(*self.neurons[-1])
+        neurons.arrange_submobjects(
+            DOWN, buff=self.neuron_to_neuron_buff
+        )
+        for neuron in neurons:
+            neuron.edges_in = VGroup()
+            neuron.edges_out = VGroup()
+        layer.neurons = neurons
+        layer.add(neurons)
+
+        if size > n_neurons:
+            dots = Tex("\\vdots")
+            dots.move_to(neurons)
+            self.neurons[-1].insert(len(neurons)//2, dots)
+            VGroup(*neurons[:len(neurons) // 2]).next_to(
+                dots, UP, MED_SMALL_BUFF
+            )
+            VGroup(*neurons[len(neurons) // 2:]).next_to(
+                dots, DOWN, MED_SMALL_BUFF
+            )
+            layer.dots = dots
+            layer.add(dots)
+        if self.brace_for_large_layers:
+            brace = Brace(layer, UP, buff=0.1)
+            brace_label = brace.get_tex(str(size)).scale(0.8)
+            layer.brace = brace
+            layer.brace_label = brace_label
+            self.braces.append((brace, brace_label))
+
+        return layer
+    # Helper method for constructor
+    def add_edges(self):
+        self.edge_groups = VGroup()
+        for l1, l2 in zip(self.layers[:-1], self.layers[1:]):
+            self.edges.append([])
+            edge_group = VGroup()
+            for n1, n2 in it.product(l1.neurons, l2.neurons):
+                edge = self.get_edge(n1, n2)
+                edge_group.add(edge)
+                n1.edges_out.add(edge)
+                n2.edges_in.add(edge)
+            self.edge_groups.add(edge_group)
+        self.add_to_back(self.edge_groups)
+    # Helper method for constructor
+    def get_edge(self, neuron1, neuron2):
+        if self.arrow:
+            return Arrow(
+                neuron1.get_center(),
+                neuron2.get_center(),
+                buff=self.neuron_radius,
+                stroke_color=self.edge_color,
+                stroke_width=self.edge_stroke_width,
+                tip_length=self.arrow_tip_size
+            )
+        self.edges[-1].append(Line(
+            neuron1.get_center(),
+            neuron2.get_center(),
+            buff=self.neuron_radius,
+            stroke_color=self.edge_color,
+            stroke_width=self.edge_stroke_width,
+        ))
+        return self.edges[-1][-1]
+    
+    # Labels each input neuron with a char l or a LaTeX character
+    def label_inputs(self, l):
+        self.output_labels = VGroup()
+        for n, neuron in enumerate(self.layers[0].neurons):
+            label = Tex(f"{l}_"+"{"+f"{n + 1}"+"}")
+            label.set_height(0.3 * neuron.get_height())
+            label.move_to(neuron)
+            self.output_labels.add(label)
+        self.add(self.output_labels)
+
+    # Labels each output neuron with a char l or a LaTeX character
+    def label_outputs(self, l):
+        self.output_labels = VGroup()
+        for n, neuron in enumerate(self.layers[-1].neurons):
+            label = Tex(f"{l}_"+"{"+f"{n + 1}"+"}")
+            label.set_height(0.4 * neuron.get_height())
+            label.move_to(neuron)
+            self.output_labels.add(label)
+        self.add(self.output_labels)
+
+    # Labels each neuron in the output layer with text according to an output list
+    def label_outputs_text(self, outputs):
+        self.output_labels = VGroup()
+        for n, neuron in enumerate(self.layers[-1].neurons):
+            label = Tex(outputs[n])
+            label.set_height(0.75*neuron.get_height())
+            label.move_to(neuron)
+            label.shift((neuron.get_width() + label.get_width()/2)*RIGHT)
+            self.output_labels.add(label)
+        self.add(self.output_labels)
+
+    # Labels the hidden layers with a char l or a LaTeX character
+    def label_hidden_layers(self, l):
+        self.output_labels = VGroup()
+        for layer in self.layers[1:-1]:
+            for n, neuron in enumerate(layer.neurons):
+                label = Tex(f"{l}_{n + 1}")
+                label.set_height(0.4 * neuron.get_height())
+                label.move_to(neuron)
+                self.output_labels.add(label)
+        self.add(self.output_labels)
