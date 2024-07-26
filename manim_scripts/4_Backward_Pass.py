e0c2656c3df3cc6d407f0aff6f9ea7ec61bc52f4
SzymonOzog <szymon.ozog@gmail.com>
2024-07-26 17:23:00 +0200

    Softmax derivative explained

--- manim_scripts/4_Backward_Pass.py
+++ manim_scripts/4_Backward_Pass.py
@@ -36,7 +36,7 @@ class NeuralNetwork(VoiceoverScene, ThreeDScene):
       self.play(Write(title))
 
     self.play(Unwrite(title))
-    nn = NeuralNetworkMobject([784, 300, 100, 10]).shift(0.1*DOWN)
+    nn = NeuralNetworkMobject([784, 300, 100, 10]).to_edge(DOWN, buff=0.05)
     idx = 0
     
     with self.voiceover(text="""In this episode, we will continue training our neural network from the previous episode,
@@ -147,19 +147,182 @@ class NeuralNetwork(VoiceoverScene, ThreeDScene):
       self.play(chain_rule[2].animate.set_color(GREEN))
 
     
-    d1 = Tex("$\\delta^L$").next_to(rect, UP)
-    d2 = Tex("$\\delta^3$").next_to(nn.neurons[-1][0], UP)
+    d1 = Tex("$\\frac{\\partial L}{\\partial a^n}$").next_to(rect, UP, aligned_edge=LEFT)
+    d2 = Tex("$\\frac{\\partial L}{\\partial x^n}$").next_to(nn.neurons[-1][0], UP)
     deltas = []
     for i in range(len(nn.neurons)):
-      deltas.append(Tex(f"$\\delta^{i}$").next_to(nn.neurons[i][0], UP))
+      deltas.append(Tex(f"$\\frac{{\\partial L}}{{\\partial x^{i}}}$").next_to(nn.neurons[i][0], UP))
 
 
     with self.voiceover(text="""That is why we call this backpropagation, we start by calculating the error at the end, and we go backwards propagating the error through our network""") as trk:
       self.play(Unwrite(chain_rule))
-      self.play(Write(d1))
+      self.play(Write(d1[0]))
       self.play(*[ShowPassingFlash(e.copy().set_color(RED), reverse_rate_function=True) for e in lines])
       self.play(Write(d2))
       for i in range(len(nn.edges)-1, -1, -1):
         self.play(*[ShowPassingFlash(e.copy().set_color(RED), reverse_rate_function=True) for e in nn.edges[i]])
         self.play(Write(deltas[i]))
     self.wait(1)
+
+    eq1 = MathTex("\\frac{\\partial L}{\\partial x^n}", "=\\frac{\\partial L}{\\partial a^n}", "\\frac{\\partial a^n}{\\partial x^n}", font_size=32).to_edge(UP, buff=0.05)
+
+    with self.voiceover(text="""To formalize, our first backpropagation equation would be that the<bookmark mark='1'/> error in the last layer
+                        is equal to the <bookmark mark='2'/> derivative of the loss with respect to the activations of the last layer, times the derivative of the activation of the last layer
+                        <bookmark mark='3'/> with respect to the inputs of that layer""") as trk:
+      self.wait_until_bookmark("1")
+      self.play(Transform(d2, eq1[0], replace_mobject_with_object_in_scene=True))
+      self.wait_until_bookmark("2")
+      self.play(Transform(d1, eq1[1], replace_mobject_with_object_in_scene=True))
+      self.wait_until_bookmark("3")
+      self.play(Write(eq1[2]))
+
+    anims = []
+    for x in nn.neurons:
+      anims.extend([Uncreate(n) for n in x])
+    for x in nn.edges:
+      anims.extend([Uncreate(n) for n in x])
+    anims.extend([Uncreate(n) for n in lines])
+    for d in deltas:
+      anims.append(Unwrite(d))
+    anims.append(Unwrite(loss))
+    anims.append(Uncreate(rect))
+    with self.voiceover(text="""Let's star by calculating the error in the last layer of our neural network""") as trk:
+      self.play(*anims)
+
+    with self.voiceover(text="""And I do have to warn you - this will be the hardest part of this episode, but bear with me
+                        as it simplifies beutifully in the end""") as trk:
+      pass
+
+    softmax = MathTex(r"q = \text{softmax}(x_i) = \frac{e^{x_i}}{\sum_{j=0}^{K} e^{x_j}}", font_size=34).next_to(eq1.get_center(), DOWN, aligned_edge=RIGHT).shift(0.5*DOWN)
+
+    cross_entropy = Tex("$H(p, q) = $","$-\\sum\\limits_{i=0}^{K}$", "$\\,p(i)$", "$\\,\\log$", "$\\,q(i)$", font_size=34).next_to(softmax, RIGHT)
+
+    with self.voiceover(text="""As a remainder, our final activation layer was using the softmax function""") as trk:
+      self.play(Write(softmax))
+
+    with self.voiceover(text="""And our loss function was the cross entropy loss""") as trk:
+      self.play(Write(cross_entropy))
+
+    with self.voiceover(text="""Let's start by calculating the derivative of the softmax with respect to the inputs""") as trk:
+      pass
+
+    s_d = Matrix([["\\frac{\\partial s_1}{\\partial x_1}", "\\frac{\\partial s_1}{\\partial x_2}", "\\cdots", "\\frac{\\partial s_1}{\\partial x_K}"],
+                 ["\\frac{\\partial s_2}{\\partial x_1}", "\\frac{\\partial s_2}{\\partial x_2}", "\\cdots", "\\frac{\\partial s_2}{\\partial x_K}"],
+                 ["\\vdots", "\\vdots", "\\ddots", "\\vdots"],
+                 ["\\frac{\\partial s_K}{\\partial x_1}", "\\frac{\\partial s_K}{\\partial x_2}", "\\cdots", "\\frac{\\partial s_K}{\\partial x_K}"]],
+                 element_alignment_corner=ORIGIN, element_to_mobject_config={"font_size": 24}, h_buff=1).next_to(softmax, DOWN)
+
+    with self.voiceover(text="""Since each element of the softmax function depends on all the other elements in the input, we have to expect
+                        the derivative to take the form of a matrix""") as trk:
+      self.play(Create(s_d))
+
+    d_eq = MathTex("\\frac{\\partial \\log(s_i)}{\\partial x_i}", "=", "\\frac{1}{s_i}", "\\frac{\\partial s_i}{\\partial x_i}", font_size=24).next_to(s_d, DOWN)
+
+
+    with self.voiceover(text="""To simplify calculating the derivative, we'll consult our new best friend - the chain rule once again""") as trk:
+      pass
+
+    with self.voiceover(text="""Using something called the log derivative trick""") as trk:
+      self.play(Write(d_eq))
+    self.wait(1)
+
+    d_eq2 = MathTex("s_i", "\\cdot", "\\frac{\\partial \\log(s_i)}{\\partial x_i}", "=", "\\frac{\\partial s_i}{\\partial x_i}", font_size=24).next_to(s_d, DOWN)
+    with self.voiceover(text="""By rearranging the equation a little bit""") as trk:
+      self.play(TransformMatchingTex(d_eq, d_eq2))
+    self.wait(1)
+
+    d_eq3 = MathTex("\\frac{\\partial s_i}{\\partial x_i}", "=", "s_i", "\\cdot", "\\frac{\\partial \\log(s_i)}{\\partial x_i}", font_size=24).next_to(s_d, DOWN)
+    with self.voiceover(text="""We can transform our derivative of a softmax element into that element multiplied by a derivative of it's logarithm""") as trk:
+      self.play(TransformMatchingTex(d_eq2, d_eq3))
+    self.wait(1)
+
+    fs = 24
+    ls = MathTex("\\log(s_i) = ", "log(\\frac{e^{x_i}}{\sum_{j=0}^{K} e^{x_j}})", font_size=fs).next_to(s_d, aligned_edge=UP)
+    ls2 = MathTex("\\log(s_i) = ", "log(e^{x_i})", " - log(\sum_{j=0}^{K} e^{x_j})", font_size=fs).next_to(s_d, aligned_edge=UP)
+    ls3 = MathTex("\\log(s_i) = ", "x_i", " - log(\sum_{j=0}^{K} e^{x_j})", font_size=fs).next_to(s_d, aligned_edge=UP)
+
+    with self.voiceover(text="""This is very helpful when calculating the derivative of softmax""") as trk:
+      self.play(Write(ls))
+
+    with self.voiceover(text="""as it helps us remove the division operator""") as trk:
+      self.play(Transform(ls, ls2))
+    self.wait(1)
+
+    with self.voiceover(text="""as well as the exponent""") as trk:
+      self.play(Transform(ls, ls3))
+    self.wait(1)
+
+    d_ls = MathTex("\\frac{\\partial \\log(s_i)} {\\partial x_k} = ", "\\frac{\\partial x_i}{\\partial x_k}", " - ", "\\frac{\\partial log(\sum_{j=0}^{K} e^{x_j})}{\\partial x_k}", font_size=fs).next_to(s_d, aligned_edge=UP)
+
+    with self.voiceover(text="""If we were to take a derivative of this with respect to an arbitary input""") as trk:
+      self.play(Transform(ls, d_ls, replace_mobject_with_target_in_scene=True))
+    self.wait(1)
+
+    t1 = MathTex(r"\begin{cases} 1 & \text{if } k = i \\ 0 & \text{if } k \neq i \end{cases}", font_size=fs, color=BLUE).next_to(d_ls, DOWN, aligned_edge=LEFT)
+    with self.voiceover(text="""The left hand side would simplify to 1 if our element index matches the derivative index and 0 otherwise""") as trk:
+      self.play(Write(t1), d_ls[1].animate.set_color(BLUE))
+
+    self.wait(1)
+
+    t2 = MathTex("\\frac{1}{\sum_{j=0}^{K} e^{x_j}}", " \\frac{\\partial \sum_{j=0}^{K} e^{x_j}}{\\partial x_k}", color=PURPLE, font_size=fs).next_to(t1, RIGHT)
+    with self.voiceover(text="""as for the right hand side, we can go for the chain rule once again!""") as trk:
+      self.play(Write(t2), d_ls[3].animate.set_color(PURPLE))
+    
+    t3 = MathTex("\\frac{1}{\sum_{j=0}^{K} e^{x_j}}", " \sum_{j=0}^{K} \\frac{\\partial e^{x_j}}{\\partial x_k}", color=PURPLE, font_size=fs).next_to(t1, RIGHT)
+    with self.voiceover(text="""We can take the sum outside of our derivative""") as trk:
+      self.play(Transform(t2, t3, replace_mobject_with_target_in_scene=True))
+
+    with self.voiceover(text="""And notice that the right hand side of this, depends on x only for one element""") as trk:
+      pass
+
+    t4 = MathTex("\\frac{1}{\sum_{j=0}^{K} e^{x_j}}", " \\cdot e^{x_k}", color=PURPLE, font_size=fs).next_to(t1, RIGHT)
+    with self.voiceover(text="""And the derivative of e^x over x is e^x""") as trk:
+      self.play(Transform(t3, t4, replace_mobject_with_target_in_scene=True))
+
+    t5 = MathTex("\\frac{e^{x_k}}{\sum_{j=0}^{K} e^{x_j}}", color=PURPLE, font_size=fs).next_to(t1, RIGHT)
+    with self.voiceover(text="""If we move it into our fraction""") as trk:
+      self.play(Transform(t4, t5, replace_mobject_with_target_in_scene=True))
+
+    t6 = MathTex("\\frac{e^{x_k}}{\sum_{j=0}^{K} e^{x_j}}", "= s_k", color=PURPLE, font_size=fs).next_to(t1, RIGHT)
+    with self.voiceover(text="""We can see that we arrived back into the softmax equation""") as trk:
+      self.play(Transform(t5, t6, replace_mobject_with_target_in_scene=True))
+
+    def monkeypatch(self, matrix):
+      ret = []
+      colormap = self.element_to_mobject_config["colormap"]
+      del self.element_to_mobject_config["colormap"]
+      for row in matrix:
+        r = []
+        for item in row:
+          r.append(self.element_to_mobject(*item, **self.element_to_mobject_config))
+          if len(r[-1].submobjects) > 2:
+            for i, c in enumerate(colormap):
+              r[-1][i].set_color(c)
+        ret.append(r)
+      return ret
+    Matrix._matrix_to_mob_matrix = monkeypatch
+    c = [GREEN, WHITE, BLUE, WHITE, PURPLE, WHITE]
+    mat = []
+    for i in range(4):
+      row = []
+      for j in range(4):
+        if i == 2 and j == 2:
+          elem = ["\\ddots"]
+        elif i == 2:
+          elem = ["\\vdots"]
+        elif j == 2:
+          elem = ["\\cdots"]
+        else:
+          elem = [f"s_{'n' if i == 3 else i+1}", "(", f"{1 if i == j else 0}", "-", f"s_{'n' if j == 3 else j+1}", ")"]
+        row.append(elem)
+      mat.append(row)
+
+    s_d2 = Matrix(mat, element_to_mobject_config={"colormap":c, "font_size": 24}, element_alignment_corner=ORIGIN).move_to(s_d)
+    with self.voiceover(text="""We can plug in all of the values and arrive with this matrix of softmax derivatives""") as trk:
+      self.play(Transform(s_d, s_d2.move_to(s_d, aligned_edge=RIGHT)), d_eq3[2].animate.set_color(GREEN))
+
+    with self.voiceover(text="""I know that this has been a lot so pause the video and look over all the components
+                        one more time to make sure that you understand where they come from""") as trk:
+      pass
+
+
