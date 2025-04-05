82e0f40864b803fd0fb01ae18b94a9389d81e057
SzymonOzog <szymon.ozog@gmail.com>
2025-04-05 11:26:25 +0200

    Fix math

--- manim_scripts/Quantization.py
+++ manim_scripts/Quantization.py
@@ -4,6 +4,10 @@ from math import radians
 
 class Quantization(Scene):
     def construct(self):
+        t = Text("QUANTIZATION").scale(3)
+        self.play(Write(t))
+        self.wait()
+
         self.play(*[FadeOut(x) for x in self.mobjects])
         #start
         ax = Axes(x_range=(-70000, 70000, 10000),
@@ -20,6 +24,7 @@ class Quantization(Scene):
         print(w,h)
         fp16_text = Text("FP16 range (-65504, 65504)").next_to(fp16_box, UP)
         self.play(ShowCreation(fp16_box), Write(fp16_text))
+        self.wait()
 
         #create q4
         w = (ax.c2p(65504, 0) - ax.c2p(-65504, 0))[0]
@@ -37,6 +42,7 @@ class Quantization(Scene):
         self.play(ShowCreation(q4_box), Write(q4_text))
         self.play(LaggedStart(*[ShowCreation(x) for x in lines]))
         q4 = VGroup(q4_box, *lines)
+        self.wait()
 
         #create values
         values = []
@@ -49,20 +55,26 @@ class Quantization(Scene):
             end_point[0] += w/16
             values.append(Line(start_point, end_point, color=ORANGE, stroke_width=4, z_index=5))
         self.play(*[ShowCreation(x) for x in values[:16]])
+        self.wait()
 
         #show scale
         self.play(q4.animate.stretch_to_fit_width(w))
+        self.wait()
 
         # not centared around 0
         self.play(*[Uncreate(x) for x in values[:8]])
+        self.wait()
 
         #more values
         self.play(*[ShowCreation(x) for x in values[16:]])
+        self.wait()
 
         #show zero point
         self.play(q4.animate.shift(w/2 * RIGHT))
+        self.wait()
 
 
+        self.wait()
         #Show quantized block
         self.play(*[FadeOut(x) for x in self.mobjects])
         block = Rectangle(width=8, height=2, color=BLUE)
@@ -73,34 +85,44 @@ class Quantization(Scene):
         w = 8
         lines=[]
         scale_blocks = [] 
-        for i in range(4):
-            start_point[0] += w/4
-            end_point[0] += w/4
+        for i in range(8):
+            start_point[0] += w/8
+            end_point[0] += w/8
             lines.append(Line(start_point, end_point, color=BLUE))
-            scale_blocks.append(Text("6Bit scale\n2Bit shift").scale(0.5).move_to(end_point + LEFT + 0.5*DOWN))
+            scale_blocks.append(Text("6Bit scale\n6Bit shift").scale(0.35).move_to(end_point + 0.5*LEFT + 0.5*DOWN))
 
         self.play(LaggedStart(*[ShowCreation(x) for x in lines]))
+        self.wait()
 
         scale_global = Text("FP16 scale & FP16 shift", color=BLUE).next_to(block, UP)
         self.play(Write(scale_global))
+        self.wait()
         self.play(LaggedStart(*[Write(x) for x in scale_blocks]))
+        self.wait()
 
         #summarize
         x = Text("32 bits").next_to(scale_global).shift(2*RIGHT)
         self.play(Write(x))
-        y = Text("4x32x8bits").next_to(lines[-1], RIGHT)
+        self.wait()
+        y = Text("256x4bits").next_to(lines[-1], RIGHT)
         center_x = x.get_center()[0]
         y_loc = y.get_center().copy()
         y_loc[0] = center_x
         y.move_to(y_loc)
         self.play(Write(y))
-        z = Text("4x8bits").next_to(scale_blocks[-1], RIGHT).shift(RIGHT)
+        self.wait()
+        z = Text("8x12bits").next_to(scale_blocks[-1], RIGHT)
+        z_loc = z.get_center().copy()
+        z_loc[0] = center_x
+        z.move_to(z_loc)
         self.play(Write(z))
+        self.wait()
         equation = [Text("+").next_to(x, DOWN).shift(0.2*DOWN),
                     Text("+").next_to(y, DOWN).shift(0.2*DOWN),
                     Text("=").next_to(z, DOWN).shift(0.2*DOWN)
                     ]
         self.play(*[Write(t) for t in equation])
 
-        result = Text(f"{32+(4*32*8)+(4*8)} bits").next_to(equation[-1], DOWN)
+        result = Text(f"{32+(4*256)+(12*8)} bits").next_to(equation[-1], DOWN)
         self.play(Write(result))
+        self.wait()
