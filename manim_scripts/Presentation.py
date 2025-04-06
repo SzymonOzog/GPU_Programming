fdf7c1994002d3b7000bef1dfa534afa69b58043
SzymonOzog <szymon.ozog@gmail.com>
2025-04-06 10:29:19 +0200

    Show timing and distance

--- manim_scripts/Presentation.py
+++ manim_scripts/Presentation.py
@@ -23,13 +23,25 @@ class Speed(Scene):
         self.play(Write(title))
 
         #write out specs
-        op = Text("Operation speed").next_to(title, DOWN).shift(2*RIGHT)
+        opsp = Text("Operation speed").next_to(title, DOWN).shift(2*RIGHT)
         mem = Text("Memory Speed").next_to(title, DOWN).shift(2*LEFT)
-        flops = Text("51.2 TFLOPS").next_to(op, DOWN)
+        flops = Text("51.2 TFLOPS").next_to(opsp, DOWN)
         bandwidth = Text("2.04 TB/s").next_to(mem, DOWN)
-        self.play(Write(op), Write(mem),
+        self.play(Write(opsp), Write(mem),
                 Write(flops), Write(bandwidth))
 
         #flops byte ratio
         breakdown = Tex("100\\frac{operations}{float}").next_to(title, DOWN).shift(2*DOWN)
         self.play(Write(breakdown))
+
+        #timings
+        clock = Text("Clock speed = 1-1.8 GHz").next_to(title, DOWN)
+        self.play(*[FadeOut(x) for x in [opsp, mem, flops, bandwidth, breakdown]])
+        self.play(Write(clock))
+
+        timing = Tex("1\\,clock\\,cycle = \\frac{2}{3} ns").next_to(clock, DOWN)
+        self.play(Write(timing))
+
+        distance = Tex("distance\\,=\\,\\frac{2}{30} mm").next_to(timing, DOWN)
+        self.play(Write(distance))
+
