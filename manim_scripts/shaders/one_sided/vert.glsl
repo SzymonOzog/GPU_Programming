ee9e9ee6a26af6b1af5921e2d951d674b6c03c1b
SzymonOzog <szymon.ozog@gmail.com>
2025-04-23 12:15:02 +0800

    One sided surface shaders

---
+++ manim_scripts/shaders/one_sided/vert.glsl
@@ -0,0 +1,28 @@
+#version 330
+
+// uniform vec3 camera_position;
+
+in vec3 point;
+in vec3 d_normal_point;
+in vec4 rgba;
+
+out vec4 v_color;
+
+#INSERT emit_gl_Position.glsl
+#INSERT get_unit_normal.glsl
+#INSERT finalize_color.glsl
+
+const float EPSILON = 1e-10;
+
+void main(){
+    emit_gl_Position(point);
+    vec3 unit_normal = normalize(d_normal_point - point);
+    if (dot(camera_position, unit_normal) < 0)
+    {
+        v_color = vec4(0);
+    }
+    else
+    {
+        v_color = finalize_color(rgba, point, unit_normal);
+    }
+}
