e216f05db80a008d57efe1193848143b29983d3d
SzymonOzog <szymon.ozog@gmail.com>
2025-07-20 09:28:18 +0200

    Disable flat verts

--- manim_scripts/shaders/one_sided/frag.glsl
+++ manim_scripts/shaders/one_sided/frag.glsl
@@ -1,6 +1,6 @@
 #version 330
 
-flat in vec4 v_color;
+in vec4 v_color;
 out vec4 frag_color;
 
 void main() {
--- manim_scripts/shaders/one_sided/vert.glsl
+++ manim_scripts/shaders/one_sided/vert.glsl
@@ -6,7 +6,7 @@ in vec3 point;
 in vec3 d_normal_point;
 in vec4 rgba;
 
-flat out vec4 v_color;
+out vec4 v_color;
 
 #INSERT emit_gl_Position.glsl
 #INSERT get_unit_normal.glsl
