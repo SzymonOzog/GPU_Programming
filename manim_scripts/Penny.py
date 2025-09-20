3a43435e9ad7c4d250743db14853420861eb39fe
SzymonOzog <szymon.ozog@gmail.com>
2025-09-20 08:02:36 +0200

    Ring gather phase

--- manim_scripts/Penny.py
+++ manim_scripts/Penny.py
@@ -16,14 +16,27 @@ class Penny(VoiceoverScene):
             RecorderService(transcription_model="base")
             # GTTSService()
             )
+        self.frame.set_euler_angles(0., 0., 0.).set_shape(15.881941, 8.926617).move_to([ 0.07937918, -0.01939364,  0.        ])
         gpus = []
+        chunks = []
+        chunks_cp = []
         dirs = [UP, RIGHT, DOWN, LEFT]
 
         for i in range(4):
             s = Square(color=GREEN)
-            t = Text(f"GPU {i}").next_to(s, UP)
+            t = Text(f"GPU {i}").move_to(s, UP).scale(0.8).shift(0.1*DOWN)
             gpu = Group(s, t).shift(3*dirs[i])
             gpus.append(gpu)
+            c = []
+            for j in range(4):
+                s2 = Rectangle(height=0.25, width=1.5)
+                t2 = Text(f"{j*4 + i}").scale(0.35).move_to(s2)
+
+                c.append(Group(s2, t2))
+            chunks.append(Group(*c).arrange(DOWN, buff=0.03).move_to(gpu).shift(0.2*DOWN))
+            chunks_cp.append(chunks[-1].copy())
+
+
 
 
         gpus = Group(*gpus)
@@ -32,8 +45,9 @@ class Penny(VoiceoverScene):
         for i in range(4):
             arcs.append(
                     ArcBetweenPoints(
+                        gpus[i-1].get_corner(dirs[i]),
                         gpus[i].get_corner(dirs[i-1]),
-                        gpus[i-1].get_corner(dirs[i])
+                        angle=-TAU/4
                         )
                     )
 
@@ -41,6 +55,42 @@ class Penny(VoiceoverScene):
         for i in range(4):
             self.add(gpus[i])
             self.add(arcs[i])
-        self.wait()
+            self.add(chunks[i])
+        # Test movement
+        for j in range(3):
+            anims = []
+            for i in range(4):
+                c = (i-j)%4
+                n = (i+1)%4
+                anims.append(chunks[i][c].animate.move_to(gpus[i].get_corner(dirs[n])))
+            self.play(*anims)
+            #arc
+            anims = []
+            for i in range(4):
+                c = (i-j)%4
+                n = (i+1)%4
+                anims.append(MoveAlongPath(chunks[i][c], arcs[n]))
+            self.play(*anims)
+            #transform
+            anims = []
+            for i in range(4):
+                c = (i-j)%4
+                n = (i+1)%4
+                print(i, n,c, chunks[n][c].submobjects[1].text)
+                new_text = f"{chunks[i][c].submobjects[1].text} + {chunks[n][c].submobjects[1].text}"
+                anims.append(
+                        AnimationGroup(
+                            Transform(chunks[n][c].submobjects[1],
+                                      Text(new_text).scale(0.35).move_to(chunks[n][c].submobjects[1])
+                                      ),
+                            Transform(chunks[i][c],
+                                      Group(Text(new_text).scale(0.35).move_to(chunks[n][c].submobjects[1])),
+                                      remover=True
+                                      ),
+                            )
+                        )
+                # somehow this doesn't get updated
+                chunks[n][c].submobjects[1].text = new_text
+            self.play(*anims)
 
 
